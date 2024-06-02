cce89494d43c4b6679f67b6390fdb0254d7adf54
makimakimek <melodiezgi17@gmail.com>
2024-06-02 17:47:17 +0300

    landing page complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import mertImage from './assets/mertImage.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <div className = "page">
+        <div className = "openingMessage">
+          Hi! I'm Mert.
+        </div>
+        
+        <div className = "paragraphAndImage">
+          <div className = "fullParagraph">
+            <div className = "paragraph">
+            I am a 3rd year PhD at the Machine Agency in HCDE, University of Washington.
+            </div>
+            <div className = "paragraph">
+            My work involves building systems that facilitate embodied knowledge sharing among domain experts and HCI researchers.
+            </div>
+            <div className = "paragraph">
+            I focus on the intersection of materials processes and bio-inspired design.
+            </div>
+            <div className = "paragraph">
+            I occasionally document fun things I create and I sporadically arrange snippets of everyday life into video stories.
+            </div>
+            <div className = "paragraph">
+            Let’s play.
+            </div>
+          </div>
+
+          <div className = "imageAndResume">
+            <div className = "imageContainer">
+              <img src = {mertImage} className = "image" alt = "an image of Mert"/>
+            </div>
+            <div className = "resume">
+              link to my Resume - 
+            </div>
+          </div>
+        </div>
+
+        <div className = "goAndNoButtons">
+          <button className = "goButton">Go!</button>
+          <button className = "noButton">No.</button>
+        </div>
+      </div>
     </>
   )
 }
